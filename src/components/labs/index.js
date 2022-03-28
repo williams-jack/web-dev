c5da6c715a8f0a45d49f9edda8738baf59af5a29
Jackson Williams <williams.jack@northeastern.edu>
2022-03-27 21:15:21 -0400

    Finished a7 lab.

--- src/components/labs/index.js
+++ src/components/labs/index.js
@@ -4,10 +4,12 @@ import Styles from "./styles";
 import ConditionalOutput from "./conditional-output";
 import TodoList from "./todo";
 import { Link } from "react-router-dom";
+import ReduxExamples from "./redux-examples";
 const Labs = () => {
     return (
         <div>
             <h1>Labs</h1>
+            <ReduxExamples />
             <TodoList />
             <ConditionalOutput />
             <Styles />
